514891148ed2ca1127e37a1ed7f4c07833db3c68
zDeltaDev <thehappydele@gmail.com>
2023-09-16 20:50:41 +0100

    updated orderbook deploy script

--- scripts/core/deployOrderBook.js
+++ scripts/core/deployOrderBook.js
@@ -1,19 +1,10 @@
 const hre = require("hardhat");
-const {
-  VAULT_ADDRESS,
-  WETH,
-  USDG_ADDRESS,
-} = require("../../constants/constants");
 
 async function main() {
-  const contract = await hre.ethers.deployContract("Router", [
-    WETH,
-    USDG_ADDRESS,
-    VAULT_ADDRESS,
-  ]);
+  const contract = await hre.ethers.deployContract("OrderBook", []);
   console.log("Deploying contract to Tenet....");
   await contract.waitForDeployment();
 
-  console.log(`Router deployed to: \nAddress: ${contract.target}`);
+  console.log(`OrderBook deployed to: \nAddress: ${contract.target}`);
 }
 main();
